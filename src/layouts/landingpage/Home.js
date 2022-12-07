a2cb2d14916232afe8e826d3ef5fe8067db33136
Joules <52895213+Joules17@users.noreply.github.com>
2022-12-07 18:14:25 -0500

    Se agrega carpeta Admin a componentes y se pone ahi la gestion de usuarios (UserList), ademas de poner proveedores en el landing

--- src/layouts/landingpage/Home.js
+++ src/layouts/landingpage/Home.js
@@ -6,7 +6,7 @@ import Box from '@mui/material/Box';
 import AppFooter from './modules/views/AppFooter';
 import ProductHero from './modules/views/ProductHero';
 import ProductValues from './modules/views/ProductValues';
-import ProductHowItWorks from './modules/views/ProductHowItWorks';
+import ProductAllies from './modules/views/ProductAllies';
 // import ProductCTA from './modules/views/ProductCTA';
 import AppAppBar from './modules/views/AppAppBar';
 import withRoot from './modules/withRoot';
@@ -20,7 +20,7 @@ function Home() {
       <Wave fill = '#fff' paused = {false} options = {{ height: 80, amplitude: 40, speed: 0.20, points: 4}}/>
       </Box>
       <ProductValues />
-      <ProductHowItWorks />
+      <ProductAllies />
       <Box component="section" sx={{ display: 'flex', background: '#fff', overflow: 'hidden' }} >
       <Wave fill = '#182848' paused = {false} options = {{ height: 80, amplitude: 40, speed: 0.20, points: 4}}/>
       </Box>
