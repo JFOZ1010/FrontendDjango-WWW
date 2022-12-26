d070b8b0687079a2c84bf65b61858ead32089653
AndresGiron <andresfelipe042001@gmail.com>
2022-12-26 13:03:46 -0500

    creando el ItemsResponse

---
+++ src/hooks/ItemsResponse.js
@@ -0,0 +1,52 @@
+import axios from 'axios'
+import { useEnv } from '../context/env.context'
+
+export const useExternalApiItem = () => {
+
+    const { apiServerUrl } = useEnv()
+
+    const makeRequest = async (options) => {
+
+        try {
+            const response = await axios(options.config)
+            const { data } = response
+
+            return data
+        } catch (error) {
+            if (axios.isAxiosError(error) && error.response) {
+                return error.response.data
+            }
+
+            return error.message
+        }
+    }
+
+    const allItems = async (setItem) => {
+
+        const config = {
+            url: `${apiServerUrl}/api/item/allItems`,
+            method: 'GET',
+            headers: {},
+            data: {}
+        }
+
+        const data = await makeRequest({ config })
+
+        // const array = []
+        // for (var i = 0; i <= data.length; i++) {
+
+        //     array.push(data[i])
+
+        // }
+
+        console.log(data)
+        setItem(data);
+
+    }
+
+
+
+    return {
+        allItems,
+    }
+}
